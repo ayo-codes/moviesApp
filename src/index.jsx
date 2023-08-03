cb9d140c3030b7f922ff756ef1bc7c241d02a71d
Ayo Onamusi <onamusia@gmail.com>
2023-08-03 23:18:17 +0100

    Movie Details Page rendering.

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import HomePage from "./pages/homePage"; // this is actually the movieListPage;
-import sample from './stories/sampleData'
+// import HomePage from "./pages/homePage"; // this is actually the movieListPage;
+import MovieDetailsPage from "./pages/movieDetailsPage" ; // named moviepage in the file
+import sample from './stories/sampleData' // imported from stories
 
 const movies = [sample, sample , sample , sample , sample , sample , sample];
 
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
+
+
 const App = () => {
   return (
-    <HomePage movies={movies} />
+    // <HomePage movies={movies} />
+
+    <MovieDetailsPage movie={sample} images={images}/>
   );
 };
 
