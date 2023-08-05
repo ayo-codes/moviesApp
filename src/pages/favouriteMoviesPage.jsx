b82513f69dd7a059739a6345e998f8318d99a510
Ayo Onamusi <onamusia@gmail.com>
2023-08-05 18:04:05 +0100

    Refactor: Favourite movie list now stored in a context.

--- src/pages/favouriteMoviesPage.jsx
+++ src/pages/favouriteMoviesPage.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
+import { MoviesContext } from "../contexts/moviesContext";
+import { useQueries } from "react-query";
+import { getMovie } from "../api/tmdb-api";
+import Spinner from "../components/spinner"
 
 const FavouriteMoviesPage = () => {
+    const { favourites : movieIds } = useContext(MoviesContext);
+
+    // Create an array of queries and run them in parallel.
+    const favouriteMovieQueries = useQueries(
+        movieIds.map((movieId) => {
+            return {
+                queryKey: ["movie", {id: movieId}],
+                queryFn : getMovie,
+            };
+        })
+    );
+
+    // Check if any of the parallel queires is still loading.
+    const isLoading = favouriteMovieQueries.find((m) => m.isLoading === true);
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+    const movies = favouriteMovieQueries.map((q) => q.data);
     const toDo = () => true; 
-    // Get movies from local storage.
-    const movies = JSON.parse(localStorage.getItem("favourites"));
+
+
+    // Get movies from local storage. changed and using contexts now
+    // const movies = JSON.parse(localStorage.getItem("favourites"));
 
     return(
         <PageTemplate 
@@ -12,7 +38,7 @@ const FavouriteMoviesPage = () => {
         movies={movies}
         selectFavouite={toDo}
         />
-    )
-}
+    );
+};
 
-export default FavouriteMoviesPage
+export default FavouriteMoviesPage;
