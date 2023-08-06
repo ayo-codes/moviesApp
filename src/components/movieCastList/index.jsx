0d883e26f7a012e9badeb4322fbceb7ffff8e718
Ayo Onamusi <onamusia@gmail.com>
2023-08-06 17:42:46 +0100

    added some centering and Heading to MovieCastList

--- src/components/movieCastList/index.jsx
+++ src/components/movieCastList/index.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import  Paper  from '@mui/material/Paper';
-import  Typography  from '@mui/material/Typography';
-
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 function MovieCastList(props) {
-  
-  let moviecast = props.moviecast
+  // sets the local moviecast to the props.moviecast object passed from the movie details parent
+
+  const moviecast = props.moviecast;
+
+  const styles = {
+    heading: {
+      display: "flex",
+    },
+    li: {
+      listStyle : "none"
+    }
+  };
 
   return (
-        <Paper> 
+    <>
+      <Typography align='center' variant="h5" component="h3" sx={styles.header}>
+        Cast Members
+      </Typography>
+      <Paper align="center" component="ul" sx={styles.li}>
         <Typography>
-        {moviecast.cast.map((cast) =>
-        (
-          <li key={cast.id}>
-            {cast.name} as {cast.character}
-          </li>
-        ))};
+          {moviecast.cast.map((cast) => (
+            <li align="center" key={cast.id} >
+              {cast.name} as {cast.character}
+            </li>
+          ))}
         </Typography>
       </Paper>
-  )
+    </>
+  );
 }
 
 export default MovieCastList;
